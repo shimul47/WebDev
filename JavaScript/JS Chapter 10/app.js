6dbcff231fc3a6f1f65fd11630b3039b19339497
shimul47 <oshimul54@gmail.com>
2024-10-26 20:46:13 +0600

    Oct 26

--- JavaScript/JS Chapter 10/app.js
+++ JavaScript/JS Chapter 10/app.js
@@ -14,9 +14,10 @@
 let btn = document.querySelector("button");
 btn.addEventListener("click", function(){
     let h3 = document.querySelector("h3");
-    h3.innerText = getRandomColor();
+    let colorX = getRandomColor();
+    h3.innerText = colorX;
     let div = document.querySelector("div");
-    div.style.backgroundColor = getRandomColor();
+    div.style.backgroundColor = colorX;
 });
 function getRandomColor(){
     let red = Math.floor(Math.random() *255);
@@ -25,3 +26,29 @@ function getRandomColor(){
     let color = `rgb(${red},${green},${blue}) `;
     return color;
 }
+
+let btn1 = document.querySelector("button");
+let h3 = document.querySelector("h3");
+let p = document.querySelector("p");
+
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor ="blue";
+    this.style.color = "white";
+}
+btn1.addEventListener("click",changeColor);
+h3.addEventListener("click",changeColor);
+p.addEventListener("click",changeColor);
+
+//keyboarad event
+let input = document.querySelector("input");
+input.addEventListener("keydown",function(){
+    console.log("key pressed");
+});
+
+//form event
+let form = document.querySelector("form");
+form.addEventListener("submit" ,function(){
+    alert("Are you sure?");
+    event.preventDefault();//will be in the same page after submit
+});
