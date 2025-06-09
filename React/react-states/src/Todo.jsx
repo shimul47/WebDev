1c18a5ea282d1779b4d0c5f17f9cb4ca29f3931b
shimul47 <oshimul54@gmail.com>
2025-06-09 10:16:37 +0600

    React 04 part2

--- React/react-states/src/Todo.jsx
+++ React/react-states/src/Todo.jsx
@@ -1,30 +1,51 @@
 import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
 export default function Todo(){
-    let [todo , setTodo] = useState([]);
+    let [todo , setTodo] = useState([{task:"Sample Task",id:uuidv4()}]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = ()=>{
-        setTodo([...todo,newTodo]);
+        setTodo((prev) =>{
+            return ([...prev,{task:newTodo,id:uuidv4()}])
+        });
         setNewTodo("");
     }
     let updateTodoValue = (event)=>{
         setNewTodo(event.target.value);
     }
-
+    let deleteTodo = (id)=>{
+        setTodo(todo.filter((i)=> i.id!=id))
+    }
+    let upperCaseTodo = (id) =>{
+        setTodo((prev) =>
+            prev.map((i) =>{
+                if(i.id == id){
+                    return{
+                        ...i,
+                        task:i.task.toUpperCase()
+                    }
+                }else{
+                    return i
+                }
+            })
+         )
+    }
 
     return(
         <div>
             <h3>Todo List</h3>
             <input type="text" placeholder="Add a Task" value={newTodo} onChange={updateTodoValue}/>
-            <button onClick={addNewTask}>Add </button>
-            <br/>
-            
+            <button onClick={addNewTask}>Add </button>              
             <h3>List :</h3>
             <hr />
             <ul>
                 {
                     todo.map((i)=>(
-                        <li>{i}</li>
+                        <li key={i.id}>
+                            <span>{i.task}</span>&nbsp;&nbsp;
+                            <span><button onClick={()=>deleteTodo(i.id)}>Delete</button></span>
+                            <span><button onClick={()=>upperCaseTodo(i.id)}>UpperCase</button></span>
+                        </li>
                     ))
                 }
             </ul>
